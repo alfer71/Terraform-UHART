50e2150b2312ea071c02d736f5b55631ab5f4fd4
Alberto Fernandez <linuxxroot@gmail.com>
2025-06-09 11:49:14 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -22,11 +22,11 @@ variable "serverlist" {
   default = {
     "dw-app-test-02.hartford.edu"  = "421a0929-f6f2-4756-ab84-a07354c80e04"
     "dw-java-test-02.hartford.edu" = "421a11c2-7494-14a3-2fb6-53f0194c5739"
-#    "uhart-dssba-001.hartford.edu" = "421a4c83-08e4-ce0f-bded-a18e09337273"
-#    "uhart-dxesa-004.hartford.edu" = "421a8a81-04bb-e2fe-e926-caf739ae240a"
-#    "uhart-dwkfa-001.hartford.edu" = "421a74c9-0097-7fb6-e0b2-f9d7e00685a8"
-#    "dw-db-test-02.hartford.edu"   = "421a56c5-77a7-79d0-c2d3-bb0d3ab08422"
-#    "uhart-dxeaa-004.hartford.edu" = "0d521a42-77af-05f7-1447-63df5badfa4b"
+    "uhart-dssba-001.hartford.edu" = "421a4c83-08e4-ce0f-bded-a18e09337273"
+    "uhart-dxesa-004.hartford.edu" = "421a8a81-04bb-e2fe-e926-caf739ae240a"
+    "uhart-dwkfa-001.hartford.edu" = "421a74c9-0097-7fb6-e0b2-f9d7e00685a8"
+    "dw-db-test-02.hartford.edu"   = "421a56c5-77a7-79d0-c2d3-bb0d3ab08422"
+    "uhart-dxeaa-004.hartford.edu" = "0d521a42-77af-05f7-1447-63df5badfa4b"
   }
 }
 
